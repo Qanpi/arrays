94944953268651383ee7b4927135889fe8df7d76
Qanpi <aleksei.terin@outlook.com>
2022-09-05 13:48:42 +0300

    all hw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         //hw1();
-        hw3();
+        hw6();
     }
 
     public static void hw1() {
@@ -28,6 +30,36 @@ public class Main {
         }
 
         System.out.println(result);
+    }
+
+    public static void hw4() {
+        int[] a = {5, 4, -7, -2, -1};
+        for (int i=a.length-1; i>=0; i--) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void hw5() {
+        int[] a = {-3, 2, 6, -11};
+        int[] s = new int[4];
+        int sum = 0;
+        for (int i=0; i<a.length; i++) {
+            sum += a[i];
+            s[i] = sum;
+        }
+
+        System.out.println(Arrays.toString(s));
+    }
+
+    public static void hw6() {
+        int[] a = {4, -3, -5, 9, 1, 11};
+        int m = 1;
+        for (int i=0; i<a.length; i++) {
+            if(a[i] < m) System.out.println(a[i]);
+        }
+        for (int i=0; i<a.length; i++) {
+            if(a[i] > m) System.out.println(a[i]);
+        }
     }
 }
 
